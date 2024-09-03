4f2e712de69db2314c2d5528b6d67d4e2d690bbd
alex-1900 <omytty@126.com>
2024-09-04 00:31:01 +0800

    update

--- src/haizeimao/index.js
+++ src/haizeimao/index.js
@@ -1,10 +1,32 @@
-const { rebootTaskCreator } = require('./control')
 const gulp = require("gulp");
 const yaml = require('yaml');
 const fs = require('fs');
 const {ROOT_PATH} = require("../constants");
 const { syncTaskCreator } = require('../lib/common');
+const { SSH_KEY } = require('../constants');
+const { ssh } = require('../lib/ssh')
 
+/**
+ * app 重启任务
+ * @param {object} sshConfig 
+ * @returns (cb: any) => Promise<void>
+ */
+function rebootTaskCreator (sshConfig) {
+  sshConfig.privateKey = SSH_KEY
+  return async (cb) => {
+    await ssh(sshConfig, [
+      'cd src/fast-bird/',
+      'ls -l'
+    ])
+    cb()
+  }
+}
+
+/**
+ * 同步所有 app
+ * @param {Array} apps 
+ * @returns 
+ */
 function parallelSyncTasks(apps) {
   const tasks = []
   for (const app of Object.values(apps)) {
@@ -17,6 +39,11 @@ function parallelSyncTasks(apps) {
   return gulp.parallel(...tasks)
 }
 
+/**
+ * 重启所有 app
+ * @param {Array} apps 
+ * @returns 
+ */
 function parallelRebootTasks(apps) {
   const tasks = []
   for (const app of Object.values(apps)) {
